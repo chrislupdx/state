aa9204315707e969d8987329d1a4dbaf9ce7c5c7
dreamer <wildetangeant@gmail.com>
2019-05-14 22:00:20 -0700

    tests passed, functionality exists

--- react-props/src/components/App.js
+++ react-props/src/components/App.js
@@ -1,33 +1,6 @@
-import React, { Component } from 'react';
-import { RandomColor } from './RandomColor';
+import React from 'react';
+import ColorPicker from './RandomColor';
 
-const colors = [];
-//css that lives in react
-//express colors as a hex?
-//express colors as 
-
-const getRandomColor = () => {
-//takes in colors array, outputs a random one
-};
-
-export default class App extends Component {
-    state = {
-      color: getRandomColor(),
-    }
-
-    selectColor = color => {
-      this.setState({ color });
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-      const { prevColor } = prevState;
-      const { color } = this.state;
-      (color === prevColor) && this.setState({ background: 'cssbasedbackgroundstuff' });
-      (color !== prevColor) && this.setState({ background: color });
-    }
-
-    render() {
-      const { background } = this.state;
-      return < RandomColor background={background} />;
-    }
+export default function App() {
+  return <ColorPicker />;
 }
